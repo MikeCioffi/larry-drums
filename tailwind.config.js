08bb8e91da61bb6e19a2a32e95d55511725970c6
Mike Cioffi <mike.cioffi95@gmail.com>
2023-11-12 22:32:34 -0600

    fix tailwind warn

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
-  darkMode: false, // or 'media' or 'class'
+  media: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
