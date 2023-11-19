419d2e7bdabc6180f0a7af44af6fe43103cbed26
Mike Cioffi <mike.cioffi95@gmail.com>
2023-11-19 12:20:09 -0600

    fix title size

--- src/App.jsx
+++ src/App.jsx
@@ -12,28 +12,34 @@ function App() {
 
       <div>      {/* homepage  */}
 
-        <div className="hidden md:flex justify-center items-center h-screen bg-cover bg-center background-desktop">
-          <div className='w-1/2'></div>
+        <div className="hidden md:flex md:flex-row justify-center items-start h-screen bg-cover bg-center background-desktop">
+          <div className='w-4/6 lg:w-1/2 h-full'> </div>
 
-          <div className="w-2/3 text-left ">
-            <h1 className=" border-slate-400 border-b-2 border-double text-9xl tracking-widest font-bold "
-
-              style={{ color: 'rgba(255, 255, 255, 0.4)' }}
-            >LARRY CIOFFI</h1>
-            <p className="  italic text-5xl tracking-wide"
-
+          <div className="w-1/4 lg:w-2/3 h-full text-left">
+            <div className='h-1/4  w-full'></div>
+            <h1 className="inline-block text-5xl md:text-7xl lg:text-9xl tracking-wide font-black	"
+              style={{
+                color: 'rgba(255, 255, 255, 0.4)',
+                borderBottom: '24px solid rgba(255, 255, 255, 0.4)'
+              }}>
+              LARRY <br /> CIOFFI
+            </h1>
+            <p className="italic text-5xl tracking-wide"
               style={{ color: 'rgba(255, 255, 255, 0.2)' }}>drummer</p>
           </div>
         </div>
 
         {/* Mobile Background */}
-        <div className="md:hidden flex flex-row justify-center items-center h-screen bg-cover bg-center background-mobile">
+        <div className="lg:hidden flex flex-row justify-center items-center h-screen bg-cover bg-center background-mobile">
           {/* Assuming the Navbar component is absolutely positioned or otherwise taken out of the normal document flow */}
           <Navbar />
           <div className="h-1/6"></div>
           {/* Text container */}
           <div className=" flex flex-col  items-end justify-start w-full h-2/3 text-center p-5">
-            <h1 style={{ color: 'rgba(255, 255, 255, 0.2)' }}
+            <h1 style={{
+              color: 'rgba(255, 255, 255, 0.4)',
+              borderBottom: '12px solid rgba(255, 255, 255, 0.4)'
+            }}
               className=" border-slate-200 border-b-2 border-double text-5xl tracking-widest font-bold">LARRY <br /> CIOFFI</h1>
             <p
               style={{ color: 'rgba(255, 255, 255, 0.2)' }}
